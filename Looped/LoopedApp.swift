c9079f9c8d51d1007650e100f27ee8934f87fe5b
Tejen Patel <tejenp@gmail.com>
2026-01-17 21:29:22 -0800

    Initial Commit

---
+++ Looped/LoopedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopedApp.swift
+//  Looped
+//
+//  Created by Tejen Patel on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct LoopedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
